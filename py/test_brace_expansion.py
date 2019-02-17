9a247d16e6c2bfe3bc808d90f10a89858a10cabb
Gaurav Chakravorty <gchak@qplum.co>
2019-02-16 21:16:35 -0500

    more test cases

--- py/test_brace_expansion.py
+++ py/test_brace_expansion.py
@@ -12,7 +12,9 @@ if __name__ == "__main__":
         '{g..a}': 'g f e d c b a',
         'a{0..3}b': 'a0b a1b a2b a3b',
         'a{b,c}d': 'abd acd',
-        '{a,b{1..3},c}': 'a b1 b2 b3 c'
+        '{a,b{1..3},c}': 'a b1 b2 b3 c',
+        'a{{0,3},2}b{h,w}': 'a0bh a0bw a3bh a3bw a2bh a2bw',
+        # 'a{{0,3}2}b{h,w}': 'a{02}bh a{02}bw a{32}bh a{32}bw' # not working yet
     }
     for input_str, exp_output in test_cases.items():
         print('Testing input string: {}'.format(input_str))
